10fc89982047abaf902c615662b9606d613e9adf
azanqureshi <azaan@tetralogicx.com>
2020-08-04 15:02:03 +0500

    azan

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -764,7 +764,7 @@ MAIN CONTENT LAYOUT
 {{--                        </ul>--}}
 {{--                    </li>--}}
                     <li class="nav-main-item">
-                        <a class="nav-main-link " href="{{route('orders.vendor.index')}}>
+                        <a class="nav-main-link " href="{{route('orders.vendor.index')}}">
                             <i class="nav-main-link-icon si si-layers"></i>
                             <span class="nav-main-link-name">Orders</span>
                         </a>
