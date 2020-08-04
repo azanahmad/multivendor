d3e4d9a919743c6691006aef045e772406a67104
azanqureshi <azaan@tetralogicx.com>
2020-08-04 15:01:25 +0500

    azan

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -748,20 +748,26 @@ MAIN CONTENT LAYOUT
                             </li>
                         </ul>
                     </li>
+{{--                    <li class="nav-main-item">--}}
+{{--                        <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">--}}
+{{--                            <i class="nav-main-link-icon si si-layers"></i>--}}
+{{--                            <span class="nav-main-link-name">Orders</span>--}}
+{{--                        </a>--}}
+{{--                        <ul class="nav-main-submenu">--}}
+{{--                            <li class="nav-main-item">--}}
+{{--                                <a class="nav-main-link" href="{{route('orders.vendor.index')}}">--}}
+{{--                                    <i class="nav-main-link-icon si si-bag"></i>--}}
+{{--                                    <span class="nav-main-link-name">All Orders</span>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
+
+{{--                        </ul>--}}
+{{--                    </li>--}}
                     <li class="nav-main-item">
-                        <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                        <a class="nav-main-link " href="{{route('orders.vendor.index')}}>
                             <i class="nav-main-link-icon si si-layers"></i>
                             <span class="nav-main-link-name">Orders</span>
                         </a>
-                        <ul class="nav-main-submenu">
-                            <li class="nav-main-item">
-                                <a class="nav-main-link" href="{{route('orders.vendor.index')}}">
-                                    <i class="nav-main-link-icon si si-bag"></i>
-                                    <span class="nav-main-link-name">All Orders</span>
-                                </a>
-                            </li>
-
-                        </ul>
                     </li>
                     <li class="nav-main-item">
                         <a class="nav-main-link " href="{{url('/')}}">
