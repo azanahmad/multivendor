0485f74b9a67644edcebbc74c80478f58532657d
azanqureshi <azaan@tetralogicx.com>
2020-08-04 12:42:43 +0500

    azan

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,19 +4,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>OneUI - Bootstrap 4 Admin Template &amp; UI Framework</title>
-
-    <meta name="description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <title>Te Sanandum, The Ibiza Boho Lifestyle</title>
+    <meta name="description" content="Te Sanandum, The Ibiza Boho Lifestyle is a Spain based Spiritual and etnical Marketplace were people can buy and sell physical and digital goods all over the world. Yoga, Boho, Jewelry, Services, Meditation, Reiki, Crystals, Gongs, Incense and much more. Join us and take your business to a new level">
     <meta name="author" content="pixelcave">
     <meta name="robots" content="noindex, nofollow">
 
     <!-- Open Graph Meta -->
-    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">
-    <meta property="og:site_name" content="OneUI">
-    <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+{{--    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">--}}
+{{--    <meta property="og:site_name" content="OneUI">--}}
+{{--    <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">--}}
+{{--    <meta property="og:type" content="website">--}}
+{{--    <meta property="og:url" content="">--}}
+{{--    <meta property="og:image" content="">--}}
 
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,12 +10,12 @@
     <meta name="robots" content="noindex, nofollow">
 
     <!-- Open Graph Meta -->
-    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">
-    <meta property="og:site_name" content="OneUI">
-    <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+{{--    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">--}}
+{{--    <meta property="og:site_name" content="OneUI">--}}
+{{--    <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">--}}
+{{--    <meta property="og:type" content="website">--}}
+{{--    <meta property="og:url" content="">--}}
+{{--    <meta property="og:image" content="">--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
